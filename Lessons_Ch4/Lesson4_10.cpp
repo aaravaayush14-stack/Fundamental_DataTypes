a349d74ab89ab85f074d31c58bb3f89254f7b771
Aayush Aarav <aarav.aayush14@gmail.com>
2026-07-20 23:46:26 +0530

    Completed lesson 4.10!

---
+++ Lessons_Ch4/Lesson4_10.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+/*
+// Demonstrating early return
+int absolute(int x)
+{
+	if (x < 0) // Also, use of if- statement 
+		return -x; // Early Return Statement
+	return x;
+}
+int main()
+{
+	std::cout << "Enter the number you want absolute value for: \n";
+	int x{};
+	std::cin >> x;
+	std::cout << absolute(x) <<'\n';
+	return 0;
+}
+*/
